c7679572c7f59688c291be8c7f2bc1e458b6ba9c
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2024-10-22 19:46:11 +0200

    Fix GitPod address replacement

--- scenario_scripts/gitpodize_zac.sh
+++ scenario_scripts/gitpodize_zac.sh
@@ -5,7 +5,8 @@ docker exec zdm-ansible-container sed -i 's/ansible_user=ubuntu/ansible_user=git
 docker exec zdm-ansible-container sed 's/home\/ubuntu/home\/gitpod/' /home/ubuntu/zdm-proxy-automation/ansible/vars/zdm_playbook_internal_config.yml -i;
 docker exec zdm-ansible-container sed 's/_user_name: ubuntu/_user_name: gitpod/' /home/ubuntu/zdm-proxy-automation/ansible/vars/zdm_playbook_internal_config.yml -i;
 
-docker exec zdm-ansible-container sed "s/hostvars\[inventory_hostname\]\['ansible_default_ipv4'\]\['address'\]/inventory_hostname/" /home/ubuntu/zdm-proxy-automation/ansible/templates/zdm_proxy_immutable_config.j2 -i;
+docker exec zdm-ansible-container sed "s/hostvars\[inventory_hostname\]\['ansible_default_ipv4'\]\['address'\]/inventory_hostname/" /home/ubuntu/zdm-proxy-automation/ansible/templates/zdm_proxy_immutable_config_file.j2 -i;
+docker exec zdm-ansible-container sed "s/hostvars\[inventory_hostname\]\['ansible_default_ipv4'\]\['address'\]/inventory_hostname/" /home/ubuntu/zdm-proxy-automation/ansible/templates/zdm_proxy_immutable_config_env_vars.j2 -i;
 
 docker exec zdm-ansible-container sed "s/hostvars\[inventory_hostname\]\['ansible_default_ipv4'\]\['address'\]/inventory_hostname/" /home/ubuntu/zdm-proxy-automation/ansible/deploy_zdm_proxy.yml -i;
 
